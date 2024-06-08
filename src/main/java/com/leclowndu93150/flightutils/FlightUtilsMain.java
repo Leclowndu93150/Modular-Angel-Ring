a5ff8d6513d519cbc6e692d55c8888275fba4cfa
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-06-08 15:53:12 +0200

    AAAAAAAAAAAAAAAAAA

--- src/main/java/com/leclowndu93150/flightutils/FlightUtilsMain.java
+++ src/main/java/com/leclowndu93150/flightutils/FlightUtilsMain.java
@@ -1,10 +1,14 @@
 package com.leclowndu93150.flightutils;
 
-import com.leclowndu93150.flightutils.curio.FlightCapability;
+import com.leclowndu93150.flightutils.items.AngelRingItem;
 import com.leclowndu93150.flightutils.registry.CreativeTabRegistry;
 import com.leclowndu93150.flightutils.registry.ItemRegistry;
+import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+import top.theillusivec4.curios.api.CuriosApi;
 
 @Mod(FlightUtilsMain.MODID)
 public class FlightUtilsMain
@@ -15,6 +19,13 @@ public class FlightUtilsMain
     {
         CreativeTabRegistry.CREATIVE_MODE_TABS.register(modEventBus);
         ItemRegistry.ITEMS.register(modEventBus);
-        modEventBus.addListener(new FlightCapability()::registerCapabilities);
+        modEventBus.addListener(AngelRingItem::registerCapabilities);
+
+        final IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        eventBus.addListener(this::setup);
+    }
+
+    private void setup(final FMLCommonSetupEvent evt) {
+        CuriosApi.registerCurio(ItemRegistry.ANGEL_RING.get(), new AngelRingItem(new Item.Properties()));
     }
 }
--- src/main/java/com/leclowndu93150/flightutils/items/AngelRingItem.java
+++ src/main/java/com/leclowndu93150/flightutils/items/AngelRingItem.java
@@ -1,8 +1,16 @@
 package com.leclowndu93150.flightutils.items;
 
+import com.leclowndu93150.flightutils.registry.ItemRegistry;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 import org.jetbrains.annotations.NotNull;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.CuriosCapability;
+import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.type.capability.ICurio;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
 public class AngelRingItem extends Item implements ICurioItem {
@@ -14,6 +22,68 @@ public class AngelRingItem extends Item implements ICurioItem {
     @Override
     public int getMaxStackSize(@NotNull ItemStack stack) {
         return 1;
+    }
+
+    private static void startFlight(Player player){
+        if(!player.isCreative() && !player.isSpectator()){
+            player.getAbilities().mayfly = true;
+            player.onUpdateAbilities();
+        }
+    }
+
+    private static void stopFlight(Player player) {
+        if (!player.isCreative() && !player.isSpectator()) {
+            player.getAbilities().flying = false;
+            player.getAbilities().mayfly = false;
+            player.onUpdateAbilities();
+        }
+    }
+
+    public static void registerCapabilities(final RegisterCapabilitiesEvent evt) {
+        evt.registerItem(
+                CuriosCapability.ITEM,
+                (stack, context) -> new ICurio() {
+
+                    @Override
+                    public ItemStack getStack() {
+                        return ItemRegistry.ANGEL_RING.toStack();
+                    }
+
+                    @Override
+                    public void curioTick(SlotContext slotContext) {
+                        if (slotContext.entity() instanceof Player player) {
+                            if (!player.getAbilities().mayfly) {
+                                startFlight(player);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public boolean canEquipFromUse(SlotContext slotContext) {
+                        return true;
+                    }
+
+                    @Override
+                    public boolean canEquip(SlotContext slotContext) {
+                        return !CuriosApi.getCuriosHelper().findEquippedCurio(ItemRegistry.ANGEL_RING.get(), slotContext.entity()).isPresent();
+                    }
+
+                    @Override
+                    public void onEquip(SlotContext slotContext, ItemStack prevStack) {
+                        if (slotContext.getWearer() instanceof Player)
+                            startFlight((Player) slotContext.getWearer());
+                    }
+
+                    @Override
+                    public void onUnequip(SlotContext slotContext, ItemStack newStack) {
+                        if (slotContext.getWearer() instanceof Player)
+                            stopFlight((Player) slotContext.getWearer());
+                    }
+
+                }, ItemRegistry.ANGEL_RING.get()
+        );
+
+
     }
 
 }
