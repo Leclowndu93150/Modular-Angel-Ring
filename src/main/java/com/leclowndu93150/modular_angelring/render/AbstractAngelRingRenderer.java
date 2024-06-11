45ef15804c3d6566f914d2629aeaf4b2f4ced761
Malo | Astri <malo1.thiebaud@gmail.com>
2024-06-11 18:29:39 +0200

    use math & cleanup

---
+++ src/main/java/com/leclowndu93150/modular_angelring/render/AbstractAngelRingRenderer.java
@@ -0,0 +1,63 @@
+package com.leclowndu93150.modular_angelring.render;
+
+import com.leclowndu93150.modular_angelring.registry.ItemRegistry;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.world.entity.player.PlayerModelPart;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.joml.Quaternionf;
+
+import static com.leclowndu93150.modular_angelring.render.AngelRingCheck.isEquipped;
+
+public abstract class AbstractAngelRingRenderer extends RenderLayer<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> {
+
+    protected static final double FLAP_FREQUENCY = 0.5; // Degrees per flap
+    protected static final double MAX_ANGLE = 25.0; // Maximum flap angle
+
+    protected double angle; // Angle of rotation
+    private final double dir; //-1 or 1, depending on wing side
+
+    public AbstractAngelRingRenderer(RenderLayerParent<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> playerModel, boolean isRightWing) {
+        super(playerModel);
+        this.angle = 0.;
+        this.dir = isRightWing ? 1. : -1.;
+    }
+
+    @Override
+    public void render(@NotNull PoseStack matrixStack, @NotNull MultiBufferSource buffer, int packedLight, AbstractClientPlayer player, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+        assert Minecraft.getInstance().player != null;
+
+        if (Minecraft.getInstance().player.getSkin().capeTexture() == null
+                && !player.isInvisible()
+                && player.isModelPartShown(PlayerModelPart.CAPE)
+                && isEquipped(player)) {
+            matrixStack.pushPose();
+            getParentModel().body.translateAndRotate(matrixStack);
+            matrixStack.translate(dir * -0.5, 0.2, 0.35);
+            matrixStack.scale((float)dir * 0.9f, 0.9f, 0.9f);
+            matrixStack.mulPose(new Quaternionf().rotateY((float) (Math.PI / 6)));
+            matrixStack.scale(-1, -1, -1);
+
+            matrixStack.translate(-0.5,0,0);
+            updateWingAngle();
+            matrixStack.mulPose(Axis.YN.rotationDegrees((float) angle));
+            matrixStack.translate(0.5,0,0);
+
+            Minecraft.getInstance().getItemRenderer().renderStatic(player, new ItemStack(ItemRegistry.ANGEL_WINGS_RIGHT.get()), ItemDisplayContext.NONE, false, matrixStack, buffer, player.level(), 0xF000F0, OverlayTexture.NO_OVERLAY, player.getId());
+            matrixStack.popPose();
+        }
+    }
+
+    private void updateWingAngle() {
+        angle = MAX_ANGLE * Math.sin(2*Math.PI * FLAP_FREQUENCY * (System.currentTimeMillis()/1000.));
+    }
+}
--- src/main/java/com/leclowndu93150/modular_angelring/render/AngelRingRendererLeft.java
+++ src/main/java/com/leclowndu93150/modular_angelring/render/AngelRingRendererLeft.java
@@ -1,65 +1,15 @@
 package com.leclowndu93150.modular_angelring.render;
 
-import com.leclowndu93150.modular_angelring.registry.ItemRegistry;
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Axis;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.player.AbstractClientPlayer;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
-import net.minecraft.client.renderer.entity.layers.RenderLayer;
-import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.world.entity.player.PlayerModelPart;
-import net.minecraft.world.item.ItemDisplayContext;
-import net.minecraft.world.item.ItemStack;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
-import org.jetbrains.annotations.NotNull;
-import org.joml.Quaternionf;
-
-import static com.leclowndu93150.modular_angelring.render.AngelRingCheck.isEquipped;
 
 @OnlyIn(Dist.CLIENT)
-public class AngelRingRendererLeft extends RenderLayer<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> {
-    private static final double FLAP_SPEED = 3.0; // Degrees per flap
-    private static final double MAX_ANGLE = 25.0; // Maximum flap angle
-    private double angle = 0.0; // Angle of rotation
-    private boolean flappingUp = true; // Direction of flapping
+public class AngelRingRendererLeft extends AbstractAngelRingRenderer {
 
     public AngelRingRendererLeft(RenderLayerParent<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> playerModel) {
-        super(playerModel);
-    }
-
-    @Override
-    public void render(@NotNull PoseStack matrixStack, @NotNull MultiBufferSource buffer, int packedLight, AbstractClientPlayer player, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
-        if (Minecraft.getInstance().player.getSkin().capeTexture() == null && !player.isInvisible() && player.isModelPartShown(PlayerModelPart.CAPE) && isEquipped(player)) {
-            matrixStack.pushPose();
-            getParentModel().body.translateAndRotate(matrixStack);
-            matrixStack.translate(0.4, 0.2, 0.4);
-            matrixStack.scale(-0.9f, 0.9f, 0.9f);
-            matrixStack.mulPose(new Quaternionf().rotateY((float) (Math.PI / 6)));
-            matrixStack.scale(-1, -1, -1);
-            updateWingAngle();
-            matrixStack.mulPose(Axis.YN.rotationDegrees((float) angle));
-            Minecraft.getInstance().getItemRenderer().renderStatic(player, new ItemStack(ItemRegistry.ANGEL_WINGS_RIGHT.get()), ItemDisplayContext.NONE, false, matrixStack, buffer, player.level(), 0xF000F0, OverlayTexture.NO_OVERLAY, player.getId());
-            matrixStack.popPose();
-        }
-    }
-
-    private void updateWingAngle() {
-        if (flappingUp) {
-            angle += FLAP_SPEED;
-            if (angle >= MAX_ANGLE) {
-                angle = MAX_ANGLE;
-                flappingUp = false;
-            }
-        } else {
-            angle -= FLAP_SPEED;
-            if (angle <= -MAX_ANGLE) {
-                angle = -MAX_ANGLE;
-                flappingUp = true;
-            }
-        }
+        super(playerModel, false);
     }
 }
