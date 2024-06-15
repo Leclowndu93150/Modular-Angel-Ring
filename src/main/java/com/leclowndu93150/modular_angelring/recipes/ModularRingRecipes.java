01cf57c5fc4153c7a683ea31640f76e0991eb989
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-06-15 12:12:00 +0200

    Improved logic

--- src/main/java/com/leclowndu93150/modular_angelring/recipes/ModularRingRecipes.java
+++ src/main/java/com/leclowndu93150/modular_angelring/recipes/ModularRingRecipes.java
@@ -1,13 +1,28 @@
 package com.leclowndu93150.modular_angelring.recipes;
 
-import net.minecraft.data.recipes.SpecialRecipeBuilder;
-import net.minecraft.world.item.crafting.CraftingBookCategory;
-import net.minecraft.world.item.crafting.Recipe;
+import com.leclowndu93150.modular_angelring.AngelRingMain;
+import com.leclowndu93150.modular_angelring.registry.ItemRegistry;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.SmithingTransformRecipeBuilder;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 
-import java.util.function.Function;
+import java.util.concurrent.CompletableFuture;
 
-public class ModularRingRecipes extends SpecialRecipeBuilder {
-    public ModularRingRecipes(Function<CraftingBookCategory, Recipe<?>> p_312708_) {
-        super(p_312708_);
+public class ModularRingRecipes extends RecipeProvider {
+
+    public ModularRingRecipes(PackOutput pOutput, CompletableFuture<HolderLookup.Provider> pRegistries) {
+        super(pOutput, pRegistries);
+    }
+
+    @Override
+    protected void buildRecipes(RecipeOutput pRecipeOutput) {
+        SmithingTransformRecipeBuilder.smithing(Ingredient.of(Items.NETHERITE_UPGRADE_SMITHING_TEMPLATE), Ingredient.of(ItemRegistry.ANGEL_RING), Ingredient.of(ItemRegistry.INERTIA_MODULE), RecipeCategory.MISC, ItemRegistry.ANGEL_RING.asItem())
+                .save(pRecipeOutput, AngelRingMain.MODID+":smithing/inertia_modifier");
+
     }
 }
